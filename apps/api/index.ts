cc45b3816df3a8b8809d166d5372814804f544f3
anshdeshwal31 <anshdeshwal0011@gmail.com>
2026-03-07 06:13:39 +0530

    add some more tests and hash the password

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -4,6 +4,7 @@ import { AuthInput } from "./types";
 import jwt from "jsonwebtoken"
 import { authMiddleware } from "./middleware";
 import type { Request, Response } from "express";
+import bcrypt from "bcrypt"
 
 const app = express();
 app.use(express.json())
@@ -18,11 +19,12 @@ app.post('/user/signup',async (req:Request,res:Response) => {
         }
         const username= data?.data?.username!
         const password = data?.data?.password!
+        const hashedPassword = await bcrypt.hash(password , 2)
 
         const user = await prisma.user.create({
             data:{
                 username,
-                password
+                password:hashedPassword
             }
         })
 
@@ -36,25 +38,31 @@ app.post('/user/signup',async (req:Request,res:Response) => {
 
 
 //  signin endpoint 
-
 app.post ("/user/signin",async (req:Request,res:Response) => { 
     try {
         const data = AuthInput.safeParse(req.body)
         if(!data.success) res.status(403).json({message: " data was in incorrect format"})
-
         const user = await prisma.user.findFirst({
-            where : {
-                username : data.data?.username,
-                password: data.data?.password
+            where:{
+                username:data.data?.username
+            },
+            select:{
+                id:true,
+                password:true
             }
         })
+        
+        if(!user?.password)res.status(401).json({message:"username was incorrect"})
+        bcrypt.compare(data.data?.password as string , user?.password as string , async (err,  result) => { 
+             if(!result) res.status(401).json({"message":"password was incorrect"})
+            
+         })
 
-        if(!user)res.status(401).json({message:"username or password was incorrect"})
 
         const jwtToken = jwt.sign({
             id:user?.id
         },process.env.JWT_SECRET!)
-
+        
         res.json({
             jwt:jwtToken
         })
@@ -69,10 +77,11 @@ app.post ("/user/signin",async (req:Request,res:Response) => {
 // endpoint to add website 
 app.post("/website",authMiddleware,async(req:Request,res:Response) => { 
     if(!req.body.url){
-        res.status(411).json({});
+        res.status(411).json({message:"url wasn't provided"});
         return;
     }
-    const user_id = req.user_id!
+    console.log("body of the request to /website: ", req.body)
+    const user_id = req.body.user_id!
 
     const website = await prisma.website.create({
         data:{
@@ -104,6 +113,8 @@ app.get("/status/:websiteId", authMiddleware,async(req:Request,res:Response) => 
         })
 
         if(!websiteInfo) res.json({message: "couldn't find the website info"})
+
+        res.json({websiteInfo})
     } catch (error) {
         
     }
--- apps/tests/website.test.ts
+++ apps/tests/website.test.ts
@@ -6,10 +6,11 @@ import { createUser } from "./testUtils";
 let BASE_URL = "http://localhost:3000";
 
 describe("Website gets created", () => {
-    let token: string
+    let token: string,id:string
     beforeAll(async () => { 
         const data = await createUser();
         token = data.jwt as string;
+        id = data.id
      })
     
     it("Website not created if url is not present", async () => {
@@ -61,6 +62,68 @@ describe("Website gets created", () => {
             
         } catch (error) {
             
+        }
+    })
+})
+
+describe("Can fetch website", () => {
+    let token1: string, userId1: string;
+    let token2: string, userId2: string;
+
+    beforeAll(async () => {
+        const user1 = await createUser();
+        const user2 = await createUser();
+        token1 = user1.jwt;
+        userId1 = user1.id;
+        token2 = user2.jwt;
+        userId2 = user2.id;
+    });
+
+    it("Is able to fetch a website that the user created", async () => {
+        try {
+            
+            const websiteResponse = await axios.post(`${BASE_URL}/website`, {
+                url: "https://google.com",
+                user_id:userId1
+
+            }, {
+                headers: {
+                    Authorization: token1
+                }
+            })
+            
+            const getWebsiteResponse = await axios.get(`${BASE_URL}/website/${websiteResponse.data.id}`, {
+                headers: {
+                    Authorization: token1
+                }
+            })
+            
+            expect(getWebsiteResponse.data.websiteInfo.id).toBe(websiteResponse.data.id)
+            expect(getWebsiteResponse.data.websiteInfo.userId).toBe(userId1)
+        } catch (error) {
+            console.log("Error in test",error)
+        }
+    })
+
+    it("Cant access website created by other user", async () => {
+        const websiteResponse = await axios.post(`${BASE_URL}/website`, {
+            url: "https://google.com", 
+            user_id:userId1
+        }, {
+            headers: {
+                Authorization: token1
+            }
+        })
+
+        try {
+            await axios.get(`${BASE_URL}/website/${websiteResponse.data.id}`, {
+                headers: {
+                    Authorization: token2
+                }
+            })
+            expect(false, "Should be able to access website of a diff user")
+        } catch(error) {
+            console.log("Error in test",error)
         }
     })
 })
